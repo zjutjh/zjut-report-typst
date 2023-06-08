09310a6c8c3ed95912d9255e4d9f472f03cd5827
Finley Ge <m13203533462@163.com>
2023-06-08 23:12:09 +0800

    chore: auto get script

--- get-template.sh
+++ get-template.sh
@@ -3,8 +3,9 @@
 echo "Downloading template..."
 
 git clone git@github.com:zjutjh/zjut-report-typst.git
-cd zjut-report-typst
-mv template ../
-rm -rf zjut-report-typst
+mv zjut-report-typst/template ./
+rm zjut-report-typst -rf
 
 echo "Done!"
+
+rm get-template.sh
